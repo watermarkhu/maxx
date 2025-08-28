5987e7d2993e06098a6ed1e3f809b1af3f160445
watermarkhu <watermarkhu@gmail.com>
2025-08-28 16:36:56 +0200

    switch to matlab test due to license

---
+++ tests/test_matlab_path.m
@@ -0,0 +1,47 @@
+classdef TestMatlabPath < matlab.unittest.TestCase
+    
+    properties
+        collection
+    end
+
+    methods (TestClassSetup)
+        function setup_pyenv(testClass)
+            [status, pythonpath] = system("uv python find");
+            if status ~= 0
+                error("python:failedToFindPython", ...
+                    "Failed to find Python in %s", projectpath);
+            end
+            pyenv('Version', strtrim(pythonpath), 'ExecutionMode', 'InProcess');
+            path = fullfile(fileparts(mfilename('fullpath')), 'files');
+            testClass.collection = py.maxx.collection.PathsCollection({path}, recursive=true);
+        end
+    end
+
+    properties (TestParameter)
+        call = {
+            'TestClass',
+            'test_function',
+            'plot_axes',
+            'test_script',
+            'ClassFolder',
+            'namespace.NamespaceClass',
+            'namespace.test_namespace_function',
+        }
+    end
+
+    methods(Test)
+        function testExample(testCase, call)
+            % Example test: verify that 1+1 equals 2
+            path_matlab = which(call);
+            testCase.verifyNotEmpty(path_matlab, sprintf('Failed to find %s in MATLAB path', call));
+            try
+                deque = testCase.collection._mapping.get(call);
+                list = py.list(deque);
+                path_python = char(py.str(list(0)));
+            catch
+                testCase.verifyFail(sprintf('Failed to find %s in Python path', call));
+            end
+            testCase.verifyEqual(path_matlab, path_python, sprintf('Paths do not match for %s', call));
+        end
+    end
+end
