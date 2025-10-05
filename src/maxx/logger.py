39ceeb4611077dc1c1426ceecbdb89d8397409f8
watermarkhu <watermarkhu@gmail.com>
2025-10-05 15:31:35 +0200

    logger

--- src/maxx/logger.py
+++ src/maxx/logger.py
@@ -7,11 +7,6 @@ from loguru import logger
 
 LogLevel = Literal["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
 
-DEFAULT_TIME_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss:SSS}</green>"
-DEFAULT_SOURCE_FORMAT = "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>"
-DEFAULT_LEVEL_FORMAT = "<level>{level: <8}</level>"
-DEFAULT_MESSAGE_FORMAT = "<level>{message}</level>"
-
 
 # Intercept standard logging messages and route them to Loguru
 # https://loguru.readthedocs.io/en/stable/overview.html#entirely-compatible-with-standard-logging
@@ -36,17 +31,7 @@ class InterceptHandler(logging.Handler):
 logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
 
 
-def configure(*, level: LogLevel = "INFO", format: str | None = None) -> None:
+def configure(*, level: LogLevel = "INFO", **kwargs) -> None:
     logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
-
-    chatty_packages: tuple[str] = ("scanner_scope",)
-    if level in ("DEBUG",):
-        for package in chatty_packages:
-            logging.getLogger(package).setLevel("INFO")
-
-    format = (
-        format
-        or f"{DEFAULT_TIME_FORMAT} | {DEFAULT_LEVEL_FORMAT} | {DEFAULT_SOURCE_FORMAT} - {DEFAULT_MESSAGE_FORMAT}"
-    )
     logger.remove()
-    logger.add(sys.stderr, colorize=True, level=level, format=format)
+    logger.add(sys.stderr, colorize=True, level=level, **kwargs)
