6db5459c29f16c8ee3c6b321df4061439a8c64c9
mahu <mark.hu@asml.com>
2025-11-06 09:19:23 +0100

    remove debug.py

--- debug.py
+++
@@ -1,8 +0,0 @@
-from maxx.collection import PathsCollection
-from pathlib import Path
-
-
-src = Path(__file__).parent.parent / "matlab_basics"
-
-# Collect all MATLAB objects from a project
-paths = PathsCollection([src], recursive=True)
